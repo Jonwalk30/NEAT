e0af770b7f22f09319e2747d882e4083597dae4a
Jonathan Walker <>
2019-01-06 18:47:28 +0000

    Added an interface to calculate the difference between two genomes

--- Species.java
+++ Species.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
-public class Species {
+public class Species implements CompatibilityDistanceCalculator {
 
   // See http://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf on determining species
   private static float c1 = 1f;
   private static float c2 = 1f;
   private static float c3 = 0.4f;
+
+  // Compatibility Distance Cutoff
+  private static float delta = 3.0f;
 
   private Agent mascot;
   private ArrayList<Agent> members;
@@ -32,8 +35,33 @@ public class Species {
   }
 
   public boolean shouldContain(Agent a) {
-    // compare the mascot to the agent using the equation
-    return true;
+    // TODO: compare the mascot to the agent using the equation
+
+    Agent a1;
+    Agent a2;
+
+    if (a.getFitness() > mascot.getFitness()) {
+      a1 = a;
+      a2 = mascot;
+    } else {
+      a1 = mascot;
+      a2 = a;
+    }
+
+    int D = disjointGeneCount(a1.getGenome(), a2.getGenome());
+
+    int E = excessGeneCount(a1.getGenome(), a2.getGenome());
+
+    float W = averageWeightDifference(a1.getGenome(), a2.getGenome());
+
+    int N = genomeSize(a1.getGenome());
+
+    float compatibilityDifference = (c1 * (float) E) / ((float) N) + (c2 * (float) D) / ((float) N) + (c3*W);
+
+    System.out.println(compatibilityDifference);
+
+    return compatibilityDifference < delta;
+
   }
 
 }
